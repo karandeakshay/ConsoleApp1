7670767371a40d95e00d33322949642c44a46966
karandeakshay <karandeakshay68@gmail.com>
2021-09-18 23:01:39 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaring Variable
+            int x, y, z;
+            double y1;
+            //user input
+            Console.WriteLine("Enter Two Number");
+
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.ReadLine(x > y && X == 5);
+        }
+    }
+}
